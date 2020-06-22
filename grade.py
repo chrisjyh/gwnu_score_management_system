ce3c56fd84551771ba90fa9954542aa6dca2c86f
chrisjyh <60378404+chrisjyh@users.noreply.github.com>
2020-06-23 00:17:33 +0900

    Add files via upload
    2차

--- grade.py
+++ grade.py
@@ -16,3 +16,36 @@ class ScoreManagementSystem:
             num += 1
             self._score[num] = Score(line.strip())
         return len(self._score)
+ def _make_cars_string(self, score,order_key,order_way):
+        result = ""
+        temp_sum=0
+        if order_way== 'asc':
+            num =1
+        elif order_way=='des':
+            num = len(score)
+
+        for key, item in score:
+            result = result + str(item.no) +','
+            result = result + item.name +','
+@@ -38,15 +40,21 @@ def _make_cars_string(self, score,order_key,order_way):
+            elif item.avg % 1 ==0:
+                result = result + str(int(item.avg)) + ','
+
+            if item.sum!=temp_sum:
+                same_num = num
+
+            if order_key =='register':
+                result = result + str(key) + '\n'
+            elif order_way== 'asc':
+                result = result + str(num) + '\n'
+                result = result + str(same_num) + '\n'
+                num +=1
+            elif order_way== 'des':
+                result = result + str(num) + '\n'
+                result = result + str(same_num) + '\n'
+                num -=1
+
+            temp_sum = item.sum
+            temp_num = num
+
+        return result.strip()
